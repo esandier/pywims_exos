5f75941f1de4c220695ec899873863c12e9194b6
esandier <esandier@yahoo.fr>
2017-03-31 18:45:52 +0200

    liste des exos

--- pywims_exos/views.py
+++ pywims_exos/views.py
@@ -1,4 +1,6 @@
 from django.shortcuts import render
+from .models import Exo
 
 def liste_exos(request):
-	return render(request, 'pywims_exos/liste_exos.html', {})
+	exos = Exo.objects.all().order_by('title')
+	return render(request, 'pywims_exos/liste_exos.html', {'exos': exos})
