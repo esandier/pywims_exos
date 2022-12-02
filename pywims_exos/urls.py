487d1722a0643304ec0e5a553aa76575f54859c7
QQ-GG <esandier@yahoo.fr>
2022-12-02 13:57:56 +0100

    update

--- pywims_exos/urls.py
+++ pywims_exos/urls.py
@@ -1,15 +1,15 @@
-﻿from django.conf.urls import url
+﻿from django.urls import re_path
 from django.conf import settings
 from . import views
 from django.conf.urls.static import static
 
 
 urlpatterns = [
-url(r'^$', views.liste_exos, name='liste_exos'),
-url(r'^exo/(?P<pk>\d+)/delete/$', views.delete_exo, name='delete_exo'),
+re_path(r'^$', views.liste_exos, name='liste_exos'),
+re_path(r'^exo/(?P<pk>\d+)/delete/$', views.delete_exo, name='delete_exo'),
 #url(r'^exo/(?P<pk>\d+)/run/$', views.run_exo, name='run_exo'),
-url(r'^exo/(?P<pk>\d+)/run-ajax/$', views.run_exo_ajax, name='run_exo_ajax'),
+re_path(r'^exo/(?P<pk>\d+)/run-ajax/$', views.run_exo_ajax, name='run_exo_ajax'),
 #url(r'^exo/(?P<pk>\d+)/corrige/$', views.corrige_exo, name='corrige_exo'),
-url(r'^exo/(?P<pk>\d+)/dev/$', views.dev_exo, name='dev_exo'),
-url(r'^exo/(?P<pk>\d+)/form/$', views.form_exo, name='form_exo')
+re_path(r'^exo/(?P<pk>\d+)/dev/$', views.dev_exo, name='dev_exo'),
+re_path(r'^exo/(?P<pk>\d+)/form/$', views.form_exo, name='form_exo')
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT) # Ajouté pour servir les fichiers FileField
